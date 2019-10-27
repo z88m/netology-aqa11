99934781e60c4d98fe45fddcb3761637149a38d6
zoom <a708yx@gmail.com>
2019-10-27 18:57:44 +0300

    added second test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    void am_900_ex_100() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void am_1000_ex_0() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
