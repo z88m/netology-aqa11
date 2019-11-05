403384f90018db8be3dc2bb0a0838aae82bfd07e
z88m <a708yx@gmail.com>
2019-11-05 17:55:35 +0300

    rebuild service (v2)

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,16 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        }
+
+        boolean isNeedMore = amount % boundary != 0;
+        if (!isNeedMore) {
+            return 0;
+        }
+
+        int remain = boundary - amount % boundary;
+        return remain;
     }
 }
